1666e2c51b1ba11d929bf779b1d40dc2cbb86a25
Michael Hill <michaelmatt98@gmail.com>
2022-04-17 16:01:02 -0500

    Reversion for testing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react'
+//import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react'
 
-function App() {
+ export default function App() {
   return (
     <div className="App">
 	<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
       </header>
-      <Authenticator />
     </div>
   );
 }
 
-export default withAuthenticator(App);
+//export default withAuthenticator(App);
